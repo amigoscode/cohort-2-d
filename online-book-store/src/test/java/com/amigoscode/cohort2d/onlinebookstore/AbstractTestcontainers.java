d8da46628d6992e5cd3e3b5db4e861073b2b8ba4
sarajaned <sarajane.doogan@gmail.com>
2023-08-09 18:11:12 +0100

    SD/feat/#55 changed to @TestPropertySource

--- online-book-store/src/test/java/com/amigoscode/cohort2d/onlinebookstore/AbstractTestcontainers.java
+++ online-book-store/src/test/java/com/amigoscode/cohort2d/onlinebookstore/AbstractTestcontainers.java
@@ -3,13 +3,14 @@ package com.amigoscode.cohort2d.onlinebookstore;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 
 @Testcontainers
-@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.datasource.url=jdbc:tc:postgresql:latest:///admin-unit-test")
 public abstract class AbstractTestcontainers {
 
     @BeforeAll
