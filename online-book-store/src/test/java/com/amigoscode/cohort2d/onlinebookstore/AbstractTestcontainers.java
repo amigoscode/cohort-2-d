9d27438a2885c23e65ae5c6fedf7c34454a71d60
sarajaned <sarajane.doogan@gmail.com>
2023-08-09 17:27:25 +0100

    SD/feat/#55 removed by mistake

---
+++ online-book-store/src/test/java/com/amigoscode/cohort2d/onlinebookstore/AbstractTestcontainers.java
@@ -0,0 +1,37 @@
+package com.amigoscode.cohort2d.onlinebookstore;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class AbstractTestcontainers {
+
+    @BeforeAll
+    static void beforeAll() {
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(
+                        postgreSQLContainer.getJdbcUrl(),
+                        postgreSQLContainer.getUsername(),
+                        postgreSQLContainer.getPassword()
+                ).load();
+        flyway.migrate();
+    }
+
+
+    @Container
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("admin-unit-test")
+                    .withUsername("admin")
+                    .withPassword("1234");
+
+
+
+}
--- online-book-store/src/test/java/com/amigoscode/cohort2d/onlinebookstore/user/UserRepositoryTest.java
+++ online-book-store/src/test/java/com/amigoscode/cohort2d/onlinebookstore/user/UserRepositoryTest.java
@@ -1,11 +1,11 @@
 package com.amigoscode.cohort2d.onlinebookstore.user;
 
+import com.amigoscode.cohort2d.onlinebookstore.AbstractTestcontainers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Collections;
 import java.util.UUID;
@@ -14,8 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("test")
-class UserRepositoryTest {
+class UserRepositoryTest extends AbstractTestcontainers {
 
     @Autowired
     private UserRepository underTest;
