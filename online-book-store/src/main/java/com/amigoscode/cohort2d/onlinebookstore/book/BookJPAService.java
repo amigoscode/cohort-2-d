2ed162dc922b01088d8373da3e9fcf7569333ae3
sarajaned <sarajane.doogan@gmail.com>
2023-08-19 21:58:50 +0100

    SD/feat#64 Added book search (title, author initially) using JPA Specification.

--- online-book-store/src/main/java/com/amigoscode/cohort2d/onlinebookstore/book/BookJPAService.java
+++ online-book-store/src/main/java/com/amigoscode/cohort2d/onlinebookstore/book/BookJPAService.java
@@ -2,9 +2,11 @@ package com.amigoscode.cohort2d.onlinebookstore.book;
 
 import com.amigoscode.cohort2d.onlinebookstore.exceptions.DuplicateResourceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -12,10 +14,11 @@ import java.util.Optional;
 public class BookJPAService implements BookDAO {
 
     private final BookRepository bookRepository;
+    private final BookSpecifications bookSpecifications;
 
     @Override
-    public List<Book> findAllBooks() {
-        return bookRepository.findAll();
+    public Page<Book> findAllBooks(Pageable pageable) {
+        return bookRepository.findAll(pageable);
     }
 
     @Override
@@ -43,7 +46,20 @@ public class BookJPAService implements BookDAO {
         bookRepository.deleteById(id);
     }
 
+    @Override
     public Book updateBook(Book book) {
         return bookRepository.save(book);
+    }
+
+    @Override
+    public Page<BookDTO> searchBooks(String query, Pageable pageable) {
+
+        Specification<Book> spec = Specification
+                .where(bookSpecifications.hasTitleLike(query))
+                .or(bookSpecifications.hasAuthorNameLike(query));
+
+        Page<Book> bookPage = bookRepository.findAll(spec, pageable);
+
+        return bookPage.map(BookDTOMapper.INSTANCE::modelToDTO);
     }
 }
--- online-book-store/src/main/java/com/amigoscode/cohort2d/onlinebookstore/book/BookRepository.java
+++ online-book-store/src/main/java/com/amigoscode/cohort2d/onlinebookstore/book/BookRepository.java
@@ -1,12 +1,14 @@
 package com.amigoscode.cohort2d.onlinebookstore.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     boolean existsBooksByIsbn(String isbn);
 
     boolean existsBookById(Long id);
+
 }
